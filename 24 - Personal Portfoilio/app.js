9d75ae1c12bec5b93b69ebe1de4f9cf9e0b59f1a
Sneha <snehakhandelwal373@gmail.com>
2024-09-28 12:21:53 +0530

    update

--- 24 - Personal Portfoilio/app.js
+++ 24 - Personal Portfoilio/app.js
@@ -1,9 +1,7 @@
-
-  const sidemenu = document.querySelector(".nav-links");
+  const sidemenu = document.querySelector("#sidemenu");
   const close = document.querySelector("#closebtn");
   const barMenu = document.querySelector("#bars");
-  const navLink = document.querySelector(".nav-item");
-
+  const navItem = sidemenu.querySelector("a");
 
   close.addEventListener("click",()=>{
     sidemenu.style.right="0";
@@ -15,9 +13,11 @@
     sidemenu.style.display.right="-200px";
   });
 
-  navLink.addEventListener("click",()=>{
+  sidemenu.addEventListener("click",()=>{
     sidemenu.style.display="none";
+    navItem.getAttribute("id","nav-item");
   });
+  
 
   const scriptURL = 'https://script.google.com/macros/s/AKfycbx3Q6BmG-mT3hKcUfYwOrvYsDbdG82rHiLAZePyJyFewBBriK3-9SJKYXV9ONJzSKueYQ/exec'
   const form = document.forms['submit-to-google-sheet']
@@ -34,4 +34,32 @@
         form.reset()
       })
       .catch(error => console.error('Error!', error.message))
-  })
+  });
+
+  const scrollRevealOption  = {
+    distance :"50px",
+    origin:"top",
+    duration:"1000",
+};
+
+  ScrollReveal().reveal(".text h2", {
+    ...scrollRevealOption,
+  });
+  
+  ScrollReveal().reveal(".text h1", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+  ScrollReveal().reveal(".text p", {
+    ...scrollRevealOption,
+    delay: 1000,
+  });
+  ScrollReveal().reveal(".links", {
+    ...scrollRevealOption,
+    delay: 1500,
+  });
+  ScrollReveal().reveal(".links ,.myimg", {
+    ...scrollRevealOption,
+    delay: 1000,
+    interval: 500,
+  });
